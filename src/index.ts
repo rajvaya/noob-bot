4f813bdcadc4d23605392110684ba191121b3575
raj vaya <rajkumarvaya@gmail.com>
2021-10-28 18:18:44 +0530

    noob bot sends Memes On new Issues 😂

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import { Probot } from "probot";
+const fetch = require('node-fetch');
 
 export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
+
+    app.log(context);
+
+     
+    const response = await fetch('https://meme-api.herokuapp.com/gimme/ProgrammerHumor');
+    const data = await response.json();
+    
+    app.log(data);
+ 
+
+    var meme = data.url;
+    
+
+
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: `![Meme](${meme})`
     });
+
     await context.octokit.issues.createComment(issueComment);
   });
   // For more information on building apps:
